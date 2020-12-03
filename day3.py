77483dfb98ec80cad60c32aeb0d8f35cbcb8c3f2
Greg Brosman <greg.brosman@gmail.com>
2020-12-02 23:03:43 -0800

    day3 added comments and logic to build from.

--- day3.py
+++ day3.py
@@ -10,7 +10,17 @@ def tree_finder():
 
         # For the length of the input list, split the data to get each line as its own list.
         for i in range(0, len(input_list)):
-            sled_run = str(input_list[i].split())
+            sled_run = input_list[i].split()[0]
+            print(sled_run)
+
+# start at index 0 of first line.
+# go to next line index 3 and record if tree
+# go to next line index 6 and record if tree
+# go to next line index 9 and record if tree
+
+# keep dropping line and add 3 to the index. check if tree and add to counter. 
+# if end of line reached, repeat from index 0 of next line.
+
 
             # Counts the trees in each line.
             tree_counter = sled_run.count('#')               
